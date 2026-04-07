09d752aa2d8b83acf21d027b38dbe266fe3a142c
Faris <fhijazi00@gmail.com>
2026-04-07 02:40:18 -0700

    debug paths

--- js/utils.js
+++ js/utils.js
@@ -501,7 +501,7 @@ function loadSceneFile( url )
     let raw = loadExternalFile( url )
 
     let scene_description = JSON.parse( raw )
-    print(scene_description)
+    console.log(scene_description)
 
     return {
 
